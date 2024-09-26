6e9645cdc93038f857844448e7ef9dd1307826f7
Sivaranjani-Techie <sivaranjani3e@gmail.com>
2024-09-26 14:56:33 +0530

    Update simple.c

--- simple.c
+++ simple.c
@@ -14,12 +14,13 @@ int main() {
     int *ptr = (int *)malloc(100 * sizeof(int));
     
     // Using uninitialized memory (valgrind will catch this)
-    int uninitialized_var;
-    //printf("uninitialized_var=%d\n", uninitialized_var);  // Error: Variable used uninitialized
+    int uninitialized_var = 0;
+    printf("uninitialized_var=%d\n", uninitialized_var);  // Error: Variable used uninitialized
     
     printf("c=%d, d=%d\n", c, d);
     
     // Memory leak: Forgot to free(ptr)
+    free(ptr);
     
     return 0;
 }
